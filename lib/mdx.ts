b4ef1dd8b8be046c2e64556f100920560b4a2e89
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-06-08 18:23:31 +0200

    Add references

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -4,11 +4,44 @@ import fs from "fs";
 import path from "path";
 import { compileMDX } from "next-mdx-remote/rsc";
 import { Frontmatter } from "./mddb";
+import { citePlugin } from "@benrbray/remark-cite";
+import remarkGfm from "remark-gfm";
+
+const bibliography = fs.readFileSync("references.bib", "utf8");
 
 export async function loadFile(slug: string) {
   const filePath = path.join(process.cwd(), "wiki", slug + ".mdx");
   return compileMDX<Frontmatter>({
     source: fs.readFileSync(filePath, "utf8"),
-    options: { parseFrontmatter: true },
+    options: {
+      parseFrontmatter: true,
+      mdxOptions: {
+        remarkPlugins: [
+          remarkGfm,
+          [
+            citePlugin,
+            {
+              // TODO: Why is this not working?
+              bibliography,
+              style: "apa",
+              linkCitations: true,
+              syntax: {
+                // see micromark-extension-cite
+                enableAltSyntax: false,
+                enablePandocSyntax: true,
+              },
+              toMarkdown: {
+                // see mdast-util-cite
+                standardizeAltSyntax: false,
+                enableAuthorSuppression: true,
+                useNodeValue: false,
+              },
+              showTooltips: true,
+              tooltipAttribute: "data-tooltip",
+            },
+          ],
+        ],
+      },
+    },
   });
 }
